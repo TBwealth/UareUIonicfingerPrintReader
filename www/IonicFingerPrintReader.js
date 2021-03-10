4072702bfc842841a3e644f2d5575591ece6b085
Temitope Badmus <tope@smartace.ng>
2021-03-10 19:32:28 +0100

    poniter

--- www/IonicFingerPrintReader.js
+++ www/IonicFingerPrintReader.js
@@ -1,4 +1,12 @@
-var exec = require('cordova/exec');
-exports.getReader = function (args,success, error) {
+// window.plugins.IonicFingerPrintReader
+
+function IonicFingerPrintReader() {
+}
+IonicFingerPrintReader.prototype.getReader = function (args,success, error) {
     exec(success, error, 'FingerPrintReader', 'getReader', [args]);
 };
+
+// var exec = require('cordova/exec');
+// exports.getReader = function (args,success, error) {
+//     exec(success, error, 'FingerPrintReader', 'getReader', [args]);
+// };
